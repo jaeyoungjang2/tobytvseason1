fe87ae900a70b2be45eaa204f28f9c97e5c3785c
jaeyoungjang2 <lion85488@gmail.com>
2023-03-22 22:11:16 +0900

    feat

--- src/main/java/com/example/tobytvseason1/Student.java
+++ src/main/java/com/example/tobytvseason1/Student.java
@@ -1,2 +1,9 @@
-package com.example.tobytvseason1;public class Student {
+package com.example.tobytvseason1;
+
+public class Student {
+
+    public static void main(String[] args) {
+        String format = String.format("%.2f", 236 / 3);
+        System.out.println(format);
+    }
 }
