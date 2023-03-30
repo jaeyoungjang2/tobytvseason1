c148a44b9528c59f63e6360f39e6ba9c8013b49c
jaeyoungjang2 <lion85488@gmail.com>
2023-03-30 19:28:37 +0900

    no message

---
+++ src/main/java/com/example/tobytvseason1/TobyTvSeason1Application.java
@@ -0,0 +1,13 @@
+package com.example.tobytvseason1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyTvSeason1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyTvSeason1Application.class, args);
+    }
+
+}
