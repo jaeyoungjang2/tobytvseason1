ce3de75b5edf226181857b6e79d19bf89a6956a8
jaeyoungjang2 <lion85488@gmail.com>
2023-04-01 21:58:38 +0900

    feat

---
+++ src/test/java/com/example/tobytvseason1/TobyTvSeason1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobytvseason1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyTvSeason1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
